d7d361b80d924b67d0f8c87c293359be6299a0f5
MohanXprofessor <140719217+MohanXprofessor@users.noreply.github.com>
2024-07-25 11:58:13 +0530

    Project compete

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btn=document.querySelector("#emoji");
+
+const emojis=["😁","😂","😎","🤩","😚","🤔","🎃","🧀","🚑","🛹","💗"];
+
+btn.addEventListener("mouseover",()=>{
+    btn.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+})
